ddf5b5ee601938c23b8f2d6f9680631d0d73b3e2
aleksbal <aleksandar.balaban@gmail.com>
2024-07-04 10:59:56 +0200

    Repository methods for geospatial search added

--- src/main/java/org/abl/aero/datasets/notam/NotamRepository.java
+++ src/main/java/org/abl/aero/datasets/notam/NotamRepository.java
@@ -1,6 +1,7 @@
 package org.abl.aero.datasets.notam;
 
 import java.util.List;
+import org.springframework.data.geo.Polygon;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.repository.query.Param;
@@ -16,10 +17,15 @@ import org.springframework.data.geo.Distance;
 @RepositoryRestResource(collectionResourceRel = "notam", path = "notam")
 public interface NotamRepository extends MongoRepository<NotamItem, String> {
 
+  List<NotamItem> findBySubject(@Param("subject") String subject);
+  List<NotamItem> findByLocation(@Param("location") String location);
   List<NotamItem> findByArea(@Param("area") String area);
 
   @Query("{'message':{'$regex':'?0','$options':'i'}}")
   Page<NotamItem> searchByMessage(String pattern, Pageable page);
+
+  @Query("{'message':{'geoWithin':'?0'}}")
+  Page<NotamItem> searchWithin(Polygon polygon, Pageable page);
 
   List<NotamItem> findByGeometryNear(Point point, Distance distance);
 }
