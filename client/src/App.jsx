0478a70af564c1b6d7a5faa7d6f15d6c92d1555f
aleksbal <10650349+aleksbal@users.noreply.github.com>
2024-11-10 20:04:06 +0100

    Bouncing effect eliminated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,28 +10,29 @@ import Tabs from './components/Tabs';
 const App = () => {
   return (
     <MapProvider>
-      <div className="flex flex-col h-screen"> {/* Full screen height for App container */}
+      {/* Full viewport height container with fixed layout */}
+      <div className="flex flex-col h-screen overflow-hidden">
 
-        {/* Top section with QueryForm */}
-        <div className="p-0 bg-gray-100 sticky top-0 z-10">
+        {/* QueryForm - Fixed height, non-sticky */}
+        <div className="p-0 bg-gray-100 z-10 h-[3rem]"> {/* Set fixed height for QueryForm */}
           <QueryForm />
         </div>
 
-        {/* Tabs section, positioned below QueryForm */}
-        <div className="bg-white shadow sticky top-[3rem] z-10">
+        {/* Tabs - Fixed height, directly below QueryForm */}
+        <div className="bg-white shadow z-10 h-[2.5rem]"> {/* Set fixed height for Tabs */}
           <Tabs />
         </div>
 
-        {/* Split pane layout that fills remaining height */}
-        <div className="flex-grow h-full">
+        {/* Split pane layout that fills remaining height without bouncing */}
+        <div className="flex-grow h-full overflow-hidden"> {/* Flex-grow to take up remaining space */}
           <Split
-            className="flex h-full overflow-hidden" // Full height within available space
+            className="flex h-full overflow-hidden"
             sizes={[15, 65, 20]}
             minSize={150}
-            gutterSize={4} // Width of the gutter between panes
+            gutterSize={4}
             direction="horizontal"
             gutterAlign="center"
-            cursor="col-resize" // Use col-resize cursor for horizontal resizing
+            cursor="col-resize"
           >
             <div className="overflow-y-auto flex flex-col h-full bg-gray-50">
               <FeatureList />
@@ -50,5 +51,6 @@ const App = () => {
 };
 
 export default App;
+
 
 
